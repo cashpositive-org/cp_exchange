1bbc7efafd863c6f871bbed57c3fd1cb95f0813b
Soumyajit Pathak <s.pathak5995@gmail.com>
2019-06-17 13:15:32 +0530

    fix port

--- packages/server/utils/config.js
+++ packages/server/utils/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
-  PORT: process.env.PORT || 4000,
+  PORT: process.env.PORT,
   SERVER_NAMESPACE: process.env.SERVER_NAMESPACE || 'express:server',
   ORIGIN: process.env.ORIGIN || 'http://localhost:3000',
   MONGO_URL: process.env.MONGO_URL,
